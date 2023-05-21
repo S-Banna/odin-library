895e10bd9403ac68e4aab27a5459ad9cfa578530
FlipFlop222 <bnsari789@gmail.com>
2023-05-21 14:49:41 +0300

    G

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, number) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function () {
-        return (this.title + " by " + this.author + ', ' + this.pages + " pages, " + this.read)
-    }
+    this.number = number
 }
 
 let formCont = document.getElementById("formCont");
@@ -18,11 +16,12 @@ function toggleForm() {
     formCont.classList.toggle("active");
 }
 
+let i = -1;
 let formSub = document.getElementById("add");
 add.addEventListener("click", makeBook);
 function makeBook() {
     event.preventDefault();
-    let titleTemp = document.getElementById("title").value;     
+    let titleTemp = document.getElementById("title").value;
     console.log(titleTemp);
     let authorTemp = document.getElementById("author").value;
     console.log(authorTemp);
@@ -30,4 +29,60 @@ function makeBook() {
     console.log(pagesTemp);
     let read = document.getElementById("is-read").checked;
     console.log(read);
+    i = i + 1;
+    console.log(i + " is i")
+    let tempBook = new Book(titleTemp, authorTemp, pagesTemp, read, i);
+    myLibrary[i] = tempBook;
+    console.log(myLibrary);
+    toggleForm();
+    display();
+    document.getElementById("title").value = '';
+    document.getElementById("author").value = '';
+    document.getElementById("pages").value = '';
+    document.getElementById("is-read").checked = false;
+}
+
+function display() {
+    let j = myLibrary.length;
+    document.getElementById("bookCont").textContent = '';
+    for (l = 0; l < j; l++) {
+        let cont = document.createElement("div");
+        document.getElementById("bookCont").appendChild(cont);
+        let titleBookTemp = myLibrary[l].title;
+        let authorBookTemp = myLibrary[l].author;
+        let pagesBookTemp = myLibrary[l].pages;
+        let isRead = myLibrary[l].read;
+        let number = myLibrary[l].number;
+        console.log(titleBookTemp + authorBookTemp + pagesBookTemp + isRead + number);
+        let titleHold = document.createElement("p");
+        titleHold.textContent = titleBookTemp;
+        cont.appendChild(titleHold);
+        let authorHold = document.createElement("p");
+        authorHold.textContent = authorBookTemp;
+        cont.appendChild(authorHold);
+        let pagesHold = document.createElement("p");
+        pagesHold.textContent = pagesBookTemp + " pages";
+        cont.appendChild(pagesHold);
+        let readButton = document.createElement("button");
+        readButton.textContent = 'Read';
+        if (isRead == true) {
+            readButton.style.backgroundColor = 'green';
+        } else {
+            readButton.style.backgroundColor = 'red';
+        }
+        readButton.addEventListener('click', () => {
+            if (myLibrary[l].read == false) {
+                readButton.style.backgroundColor = 'green';
+                myLibrary[l].read = true;
+            } else {
+                readButton.style.backgroundColor = 'red';
+                myLibrary[l].read = false;
+            }
+        });
+        cont.appendChild(readButton);
+        let removeButton = document.createElement("button");
+        removeButton.textContent = 'Remove';
+        removeButton.style.backgroundColor = 'grey';
+        cont.appendChild(removeButton);
+    }
 }
