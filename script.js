8bde6fcbc024ccaf863611ca91dc4cdc1e489c8a
FlipFlop222 <bnsari789@gmail.com>
2023-05-21 16:58:43 +0300

    FINALLY WORKS

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+let l;
 
 function Book(title, author, pages, read, number) {
     this.title = title
@@ -6,6 +7,7 @@ function Book(title, author, pages, read, number) {
     this.pages = pages
     this.read = read
     this.number = number
+    this.class = false
 }
 
 let formCont = document.getElementById("formCont");
@@ -40,48 +42,84 @@ function display() {
     let j = myLibrary.length;
     document.getElementById("bookCont").textContent = '';
     for (let l = 0; l < j; l++) {
-        let cont = document.createElement("div");
-        document.getElementById("bookCont").appendChild(cont);
-        let titleBookTemp = myLibrary[l].title;
-        let authorBookTemp = myLibrary[l].author;
-        let pagesBookTemp = myLibrary[l].pages;
-        let isRead = myLibrary[l].read;
-        let number = myLibrary[l].number;
-        let titleHold = document.createElement("p");
-        titleHold.textContent = titleBookTemp;
-        cont.appendChild(titleHold);
-        let authorHold = document.createElement("p");
-        authorHold.textContent = authorBookTemp;
-        cont.appendChild(authorHold);
-        let pagesHold = document.createElement("p");
-        pagesHold.textContent = pagesBookTemp + " pages";
-        cont.appendChild(pagesHold);
-        let readButton = document.createElement("button");
-        readButton.textContent = 'Read';
-        if (isRead == true) {
-            readButton.style.backgroundColor = 'green';
+        if (myLibrary[l] == undefined) {
+
         } else {
-            readButton.style.backgroundColor = 'red';
-        }
-        cont.appendChild(readButton);
-        let removeButton = document.createElement("button");
-        removeButton.textContent = 'Remove';
-        removeButton.style.backgroundColor = 'grey';
-        cont.appendChild(removeButton);
-        readButton.addEventListener('click', () => {
-            if (myLibrary[l].read == false) {
-                readButton.style.backgroundColor = 'green';
-                myLibrary[l].read = true;
+            if (myLibrary[l].class == false) {
+                let cont = document.createElement("div");
+                document.getElementById("bookCont").appendChild(cont);
+                let titleBookTemp = myLibrary[l].title;
+                let authorBookTemp = myLibrary[l].author;
+                let pagesBookTemp = myLibrary[l].pages;
+                let isRead = myLibrary[l].read;
+                let number = myLibrary[l].number;
+                let titleHold = document.createElement("p");
+                titleHold.textContent = titleBookTemp;
+                cont.appendChild(titleHold);
+                let authorHold = document.createElement("p");
+                authorHold.textContent = authorBookTemp;
+                cont.appendChild(authorHold);
+                let pagesHold = document.createElement("p");
+                pagesHold.textContent = pagesBookTemp + " pages";
+                cont.appendChild(pagesHold);
+                let readButton = document.createElement("button");
+                readButton.textContent = 'Read';
+                if (isRead == true) {
+                    readButton.style.backgroundColor = 'green';
+                } else {
+                    readButton.style.backgroundColor = 'red';
+                }
+                cont.appendChild(readButton);
+                let removeButton = document.createElement("button");
+                removeButton.textContent = 'Remove';
+                removeButton.style.backgroundColor = 'grey';
+                cont.appendChild(removeButton);
+                readButton.addEventListener('click', () => {
+                    if (myLibrary[l].read == false) {
+                        readButton.style.backgroundColor = 'green';
+                        myLibrary[l].read = true;
+                    } else {
+                        readButton.style.backgroundColor = 'red';
+                        myLibrary[l].read = false;
+                    }
+                });
+                removeButton.addEventListener('click', () => {
+                    let removed = document.getElementById("bookCont");
+                    let removal = removed.children.item(l);
+                    myLibrary[l].class = true;
+                    test();
+                })
             } else {
-                readButton.style.backgroundColor = 'red';
-                myLibrary[l].read = false;
+                let cont = document.createElement("div");
+                document.getElementById("bookCont").appendChild(cont);
+                let titleBookTemp = myLibrary[l].title;
+                let authorBookTemp = myLibrary[l].author;
+                let pagesBookTemp = myLibrary[l].pages;
+                let isRead = myLibrary[l].read;
+                let number = myLibrary[l].number;
+                let titleHold = document.createElement("p");
+                titleHold.textContent = titleBookTemp;
+                cont.appendChild(titleHold);
+                let authorHold = document.createElement("p");
+                authorHold.textContent = authorBookTemp;
+                cont.appendChild(authorHold);
+                let pagesHold = document.createElement("p");
+                pagesHold.textContent = pagesBookTemp + " pages";
+                cont.appendChild(pagesHold);
+                let readButton = document.createElement("button");
+                let removed = document.getElementById("bookCont");
+                removed.children.item(l).classList.add("hide");
             }
-        });
-        removeButton.addEventListener('click', () => {
-            console.log(myLibrary);
-            myLibrary.splice(l, 1);
-            console.log(myLibrary);
-            display();
-        })
+        }
+    }
+}
+
+function test() {
+    for (let x = 0; x < myLibrary.length; x++) {
+        let removed = document.getElementById("bookCont");
+        // let removal = removed.children.item(x);
+        if (myLibrary[x].class == true) {
+            removed.children.item(x).classList.add("hide");
+        }
     }
 }
